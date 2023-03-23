42408cbd504938fd7d4f44434a28766ceca2e329
«Maria» <chalova_2111@mail.ru>
2023-03-23 12:17:49 +0500

    005

---
+++ 005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userneme = Console.ReadLine();
+
+if(userneme.ToLower() == "Mаша")
+{
+     Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Ура, это же МАША!");
+    Console.WriteLine(userneme);
+}
