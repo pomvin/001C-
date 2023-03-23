99e4d080d1e4a71e03e07f276dbcaccba5c4b2a2
«Maria» <chalova_2111@mail.ru>
2023-03-23 12:45:18 +0500

    007

---
+++ 007/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+int xa = 35;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 70;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while(count < 10000)
+{
+    int whot = new Random().Next(0, 3);
+    if(whot ==0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (whot == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (whot == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+}
